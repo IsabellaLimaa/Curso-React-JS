404f8e79a8f7940537eb303a23d23445cf58dcd9
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-10-08 15:50:38 -0300

    Update app1.js
    Aprendendo operadores matemáticos em JS.

---
+++ JavaScript/1 - Operadores matemáticos/app1.js
@@ -0,0 +1,13 @@
+console.log('Isabella Lima'); //exibe um valor no terminal
+
+//somar
+console.log('A soma é: ', 10 + 3);
+
+//subtrair
+console.log('A subtração é: ', 10 - 3);
+
+//dividir
+console.log('A divisão é: ', 10 / 3);
+
+//multiplicação
+console.log('A soma é: ', 10 * 3);
