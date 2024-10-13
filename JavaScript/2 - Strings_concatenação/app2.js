57614f6b37a60387af773e632161839b7f477873
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-10-13 12:34:17 -0300

    Create app2.js
    Aprendendo sobre concatenação.

---
+++ JavaScript/2 - Strings_concatenação/app2.js
@@ -0,0 +1,14 @@
+// strings: letras, números, caracteres diversos, palavras e textos
+
+console.log("Isabella Lima");
+console.log("10");
+console.log("10 + 10"); //não é string
+console.log(10 + 10); //não é string
+
+//concatenação 
+console.log("A minha idade é: " + 21);
+
+//concatenação com  template string ou tamplate literals
+console.log(`A minha idade é ${21}`);
+
+console.log(`Essa é uma alternativa ${true}`);
