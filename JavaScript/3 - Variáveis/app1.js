fe9f9d3390f94787df2db0204704cc12e91f6125
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-10-13 12:46:34 -0300

    Create app1.js
    Aprendendo sobre variável.

---
+++ JavaScript/3 - Variáveis/app1.js
@@ -0,0 +1,9 @@
+//variáveis em Javascript: são maneiras de amarzenarmos informações para usarmos posteriormente
+
+//VAR: Maneira antiga
+//permite que add um valor nele e depois poder alterar esse valor. 
+
+//LET: Adiciona um valor e também pode alterar.
+
+//CONST: Adiciona um valor e não pode mais substituir.
+
