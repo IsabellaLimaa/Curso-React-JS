acaffbe71e3e2169c6d85f016e98f44525f468df
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-10-21 15:43:20 -0300

    Números
    Aprendendo sobre números.

---
+++ JavaScript/4 - Números/app1.js
@@ -0,0 +1,6 @@
+//float, double, integers
+
+//casas decimais são separadas por ponto e não por vírgulas.
+
+console.log(10+11.333333333333);
+console.log(12,3 + 13,3);
---
+++ JavaScript/5 - Booleanos/app1.js
@@ -0,0 +1,8 @@
+const dormindo = false;
+const inscrito = true;
+
+const dormindoString = "true"; //não é booleano dentro de aspas.
+
+console.log(dormindo);
+console.log(inscrito);
+console.log(dormindoString);
