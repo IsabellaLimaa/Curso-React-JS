a5a07e4d95095951be93ec7c1f3bdb1c2e86cbe7
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-11-15 18:11:42 -0300

    While
    Aprendendo o laço While.

---
+++ JavaScript/15 - While/app1.js
@@ -0,0 +1,19 @@
+/*let user = "Isabella";
+let number = 0;
+
+while(user === "Isabella"){
+    console.log("User é Isabella");
+    number++;
+    console.log(number);
+
+    if(number >= 10){
+        user = "João";
+    }
+}*/
+
+let number = 0;
+//le o While como "Enquanto"
+while(number <= 10){
+    number++;
+    console.log(number);
+}
