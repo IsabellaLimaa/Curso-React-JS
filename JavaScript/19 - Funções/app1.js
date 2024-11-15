0228dc21f76d58a17600b958e55bc439faa8def6
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-11-15 18:53:03 -0300

    Funções
    Aprendendo sobre funções.

---
+++ JavaScript/19 - Funções/app1.js
@@ -0,0 +1,27 @@
+//Reaproveitar código 
+
+/*console.log("Função sem parâmetro");
+//funçção sem parâmetro
+function mostrarMeuNome(){
+    console.log("Isabella");
+}
+
+mostrarMeuNome();
+
+console.log("Função com parâmetro: ");
+
+//função com parâmetro
+function showOneName(name){
+    console.log(name);
+}
+
+showOneName("Isabella");
+showOneName("Chico");
+showOneName("Alice");
+showOneName("Isabele");*/
+
+function somNumeros (n1, n2, n3){
+    console.log(n1 + n2 + n3);
+}
+
+somNumeros(10, 20, 30);
