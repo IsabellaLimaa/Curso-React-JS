7fccbc875af4dcebb37df21e6496e7d1b53746bc
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-11-18 20:38:36 -0300

    métodos
    Aprendendo Length, push e pop.

---
+++ JavaScript/22 - length, push e pop/app1.js
@@ -0,0 +1,15 @@
+const pessoas = ["Isabella", "Chico"];
+
+//lenght mostra o tamanho do array atualmente
+console.log(pessoas.length, "pessoas.length");
+
+console.log(pessoas[2], "posição 2");
+
+//push adiciona um elemento no final do array
+pessoas.push("Alice");
+console.log(pessoas[2], "posição 2");
+
+//pop remove o ultimo elemento do array
+pessoas.pop();
+console.log(pessoas[2], "posição 2");
+
