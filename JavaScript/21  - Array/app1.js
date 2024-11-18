66959d742be36bb229b492eb4da36c21a5cef89c
Isabella Lima <152313013+IsabellaLimaa@users.noreply.github.com>
2024-11-18 20:28:41 -0300

    Array
    Introdução à Array.

---
+++ JavaScript/21  - Array/app1.js
@@ -0,0 +1,15 @@
+//                 0            1        2         3
+const pessoas = ["Isabella", "Chico", "Alice", "Isabele"];
+
+console.log(pessoas.length);//Array com 4 posições
+console.log(pessoas[2]);
+
+for (let pessoa of pessoas){
+    console.log(pessoa);
+}
+
+const numeros = [1, 2, 3, 4 ,5 ,6];
+
+for (const n of numeros){
+    console.log(n);
+}
